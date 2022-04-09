43a29b23a9286f96ec858612436fd68c11043343
Nawodawi <nawodawijebandara7@gmail.com>
2022-04-10 02:31:09 +0530

    changes to uRLS

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
 b2cauth.initialize({
-    instance: "https://login.microsoftonline.com/6ae2c896-b2ab-435f-8566-cb73809426a9",
+    instance: "https://login.microsoftonline.com/tfp/",
     // tenant: "uoksetu.onmicrosoft.com",
     tenant: "distributedproject.onmicrosoft.com",
     signInPolicy: "B2C_1_react_signup",
     // applicationId: "1278766e-5c26-4964-842d-6f5f50494992",
-    applicationId: "eac171cc-f507-4db3-837b-1b9b1c36514d",
+    applicationId: "998cf93e-6404-42e8-80a4-bab5c302d432",
     cacheLocation: "sessionStorage",
     // scopes: ["https://uoksetu.onmicrosoft.com/api/user_impersonation"],
     scopes: ["https://distributedproject.onmicrosoft.com/api/user_impersonation"],
