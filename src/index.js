d1ad22dd22ff8de15f6dafc7bea2a25b28964ef6
Nawodawi <nawodawijebandara7@gmail.com>
2022-04-09 18:19:45 +0530

    all

--- src/index.js
+++ src/index.js
@@ -7,11 +7,14 @@ import * as serviceWorker from "./serviceWorker";
 
 b2cauth.initialize({
     instance: "https://login.microsoftonline.com/tfp/",
-    tenant: "uoksetu.onmicrosoft.com",
+    // tenant: "uoksetu.onmicrosoft.com",
+    tenant: "distributedproject.onmicrosoft.com",
     signInPolicy: "B2C_1_react_signup",
-    applicationId: "1278766e-5c26-4964-842d-6f5f50494992",
+    // applicationId: "1278766e-5c26-4964-842d-6f5f50494992",
+    applicationId: "5da457d6-7ec6-4433-9d56-4490a8c1ecb7",
     cacheLocation: "sessionStorage",
-    scopes: ["https://uoksetu.onmicrosoft.com/api/user_impersonation"],
+    // scopes: ["https://uoksetu.onmicrosoft.com/api/user_impersonation"],
+    scopes: ["https://distributedproject.onmicrosoft.com/api/user_impersonation"],
     redirectUri: window.location.origin,
     postLogoutRedirectUri: window.location.origin,
 });
